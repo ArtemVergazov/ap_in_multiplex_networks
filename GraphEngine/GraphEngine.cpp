6661fdfe8843f4202dbe773b9847c1fd660f570f
Artem <kuznechik0810@gmail.com>
2024-03-18 14:14:13 +0300

    hyperbolic percolation and rgcc vs. g plots

--- GraphEngine/GraphEngine.cpp
+++ GraphEngine/GraphEngine.cpp
@@ -11,36 +11,52 @@ int main(int argc, char **argv) {
 
     auto begin = std::chrono::high_resolution_clock::now();
 
-    constexpr int N = 1000;
+    constexpr int N = 5000;
     constexpr double cMin = .01;
     constexpr double cMax = 12;
     constexpr int cNum = 200;
-    constexpr int nRuns = 1000;
+    constexpr int nRuns = 100;
     constexpr double gamma1 = 3;
     constexpr double gamma2 = 3;
     constexpr double T1 = .4;
     constexpr double T2 = .4;
     constexpr double c = 8;
     constexpr double nu = 1;
+    constexpr double g = 1;
     constexpr double gMin = 0;
     constexpr double gMax = 1;
     constexpr int gNum = 10;
 
     std::string project_dir = "C:\\Users\\kuzne\\Documents\\ACTIVE_PROJECTS\\research\\";
     std::string outputFileName = project_dir +
-        //"\\experiments\\hyperbolic\\correlation_percolation\\results_nu_1.json";
-        "\\experiments\\articulation_points_monoplex\\results\\hyperbolic\\results_hyperbolic.json";
+        "\\experiments\\hyperbolic\\correlation_percolation\\results_nu_1.json";
+        //"\\experiments\\articulation_points_monoplex\\results\\hyperbolic\\results_hyperbolic_5000.json";
 
-    MonoplexPercolationRunBase *monoplexRun = new MonoplexPercolationRunHyperbolic(
-        N, cMin, cMax, cNum,
-        nRuns, outputFileName,
-        gamma1, T1
-    );
-    monoplexRun->run();
-    delete monoplexRun;
-    //runMultiplexAngularCorrelationPercolationHyperbolic(
-    //    N, c, nu, gMin, gMax, gNum, gamma1, gamma2, T1, T2, nRuns, outputFileName
+    //MonoplexPercolationRunBase *run = new MonoplexPercolationRunHyperbolic(
+    //    N, cMin, cMax, cNum,
+    //    nRuns, outputFileName,
+    //    gamma1, T1
     //);
+
+    //MultiplexPercolationRunBase *run = new MultiplexPercolationRunHyperbolic(
+    //    N, cMin, cMax, cNum,
+    //    nRuns, outputFileName,
+    //    gamma1, gamma2,
+    //    T1, T2,
+    //    nu, g
+    //);
+
+    //run->run();
+    //delete run;
+
+    runMultiplexAngularCorrelationPercolationHyperbolic(
+        N, c, nu,
+        gMin, gMax, gNum,
+        gamma1, gamma2,
+        T1, T2,
+        nRuns,
+        outputFileName
+    );
 
     auto end = std::chrono::high_resolution_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
