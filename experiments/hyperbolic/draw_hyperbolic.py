34a42bddf4d30402649707260cba94e1af67569e
Artem <kuznechik0810@gmail.com>
2024-05-24 13:43:31 +0300

    coherence plot and update real hyperbolic plot

--- experiments/hyperbolic/draw_hyperbolic.py
+++ experiments/hyperbolic/draw_hyperbolic.py
@@ -12,11 +12,9 @@ def find_component(node, connected_components):
 
 
 def cascade_remove_links(G):
-
     keep_going = True
     while keep_going:
         keep_going = False
-
         clusters_in_layers = [list(nx.connected_components(g)) for g in G]
 
         for i, g in enumerate(G):
@@ -42,83 +40,55 @@ def max_comp(g):
     return max(comps, key=len)
 
 
-def draw_hyperbolic_monoplex(links, r, theta, ax=None):
-    plt.rc('text', usetex=True)
-    plt.rc('font', size=22, **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
-    plt.rcParams['xtick.major.pad'] = 8
-    plt.rcParams['ytick.major.pad'] = 8
-
-    if ax is None:
-        fig = plt.figure()
-        fig.set_size_inches(5, 5)
-
-        ax = fig.add_subplot(1, 1, 1, projection='polar')
-    else:
-        fig = ax.figure
-
+def draw_hyperbolic_monoplex(links, r, theta, ax, ms=1.5):
     print('\nStarting AP removal')
 
     r = np.array(r)
     theta = np.array(theta)
-
     G = [nx.from_edgelist(links)]
 
     for i, j in links:
-        ax.plot(
-            [theta[i], theta[j]], [r[i], r[j]], '-',
-            color='k', linewidth=.01, alpha=.2, label=None,
-        )
+        ax.plot([theta[i], theta[j]], [r[i], r[j]], '-', color='k', linewidth=.01, alpha=.2, label=None)
 
     it = 1
     while True:
         print(f'AP removal: iteration {it}')
         G = cascade_remove_links(G)
-
-        removed_at_this_iteration = []
-
         gcc = max_comp(G[0])
-        for g in G:
-            to_remove = [n for n in g if n not in gcc]
-            g.remove_nodes_from(to_remove)
-            removed_at_this_iteration.extend(to_remove)
+        G[0].remove_nodes_from([n for n in G[0] if n not in gcc])
 
         art_points = articulation_points_multiplex(G)
 
-        for g in G:
-            g.remove_nodes_from(art_points)
-        removed_at_this_iteration.extend(art_points)
-        removed_at_this_iteration = list(set(removed_at_this_iteration))
+        G[0].remove_nodes_from(art_points)
 
         print(f'AP removal: {len(art_points)} APs')
-        print(f'AP removal: removed {len(removed_at_this_iteration)} nodes')
-
-        ax.plot(
-            theta[removed_at_this_iteration], r[removed_at_this_iteration], 'o',
-            markersize=1.5,
-            alpha=.83,
-            color='tab:blue',
-            # label=fr'$\mathrm{{Removed\ at\ iteration\ {it}}}$',
-        )
         it += 1
-
         if not art_points:
             break
 
-    rgb_nodes = [n for n in G[0]]
+    rgb_nodes = list(G[0].nodes)
+
     ax.plot(
-        theta[list(rgb_nodes)], r[list(rgb_nodes)], 'o',
-        markersize=1.5,
+        np.delete(theta, rgb_nodes), np.delete(r, rgb_nodes), 'o',
+        markersize=ms,
         alpha=.83,
-        color='#f78fb3',
-        label=fr'$\mathrm{{RGCC}}$',
+        label=None,
+        color='orange',
+    )
+
+    BRIGHT_MAGENTA = '#ff08e8'
+    ax.plot(
+        theta[rgb_nodes], r[rgb_nodes], 'o',
+        markersize=ms,
+        alpha=.83,
+        color=BRIGHT_MAGENTA,
+        label=None,
     )
 
     ax.grid(False)
     ax.spines['polar'].set_visible(False)
     ax.set_yticklabels([])
     ax.set_xticklabels([])
-
-    fig.tight_layout()
 
 
 def draw_hyperbolic_multiplex(links1, links2, r1, r2, theta1, theta2, ax1, ax2, ms=1.5):
@@ -158,7 +128,6 @@ def draw_hyperbolic_multiplex(links1, links2, r1, r2, theta1, theta2, ax1, ax2, 
 
         print(f'AP removal: {len(art_points)} APs')
         it += 1
-
         if not art_points:
             break
 
